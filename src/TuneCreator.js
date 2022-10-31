ac7bbaffabdbf84592c3b34f0c56e71c4faf401e
Rob Salethorn <rob.salethorn@placeholder.com>
2022-10-31 02:50:49 +0000

    Adding existing project to Git

---
+++ src/TuneCreator.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import './App.css';
+import Stylophone from './Stylophone'
+import NotesPage from './NotesPage'
+import NotesLine from './NotesLine';
+import ErrorBoundary from './ErrorBoundary';
+
+class TuneCreator extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.addNoteToSong = this.addNoteToSong.bind(this);
+        this.removeNoteFromSong = this.removeNoteFromSong.bind(this);
+
+        this.selectBeat = this.selectBeat.bind(this);
+
+        this.onGroupSelectStart = this.onGroupSelectStart.bind(this);
+        this.onGroupSelectEnd = this.onGroupSelectEnd.bind(this);
+
+        this.state = {
+            song: [],
+            selectedBeat: null,
+            isGroupSelect: false,
+            startGroupSelectedBeat: null,
+            endGroupSelectedBeat: null
+        };
+    }
+    render() {
+        return (
+        <div className="tunecreator-container">
+            <Stylophone addNoteToSong={this.addNoteToSong}/>
+            <ErrorBoundary>
+                <NotesPage 
+                    notes={this.state.song} 
+                    removeNoteFromSong={this.removeNoteFromSong} 
+                    selectBeat={this.selectBeat} 
+                    selectedBeat={this.state.selectedBeat} 
+                    onGroupSelectStart={this.onGroupSelectStart}
+                    onGroupSelectEnd={this.onGroupSelectEnd}
+                    isGroupSelect={this.state.isGroupSelect}
+                    startGroupSelectedBeat={this.state.startGroupSelectedBeat}
+                    endGroupSelectedBeat={this.state.endGroupSelectedBeat}
+                />
+            </ErrorBoundary>
+        </div> );
+    }
+    addNoteToSong(e) {
+        let start = e.target.classList[1].indexOf("-")+1;
+        let end = e.target.classList[1].length;
+        let keyNo = e.target.classList[1].substr(start, end);
+
+        let newSong = this.state.song;
+        if (this.state.selectedBeat == null) {
+            newSong.push(keyNo);
+            this.setState({song: newSong});
+        } else {
+            newSong.splice(this.state.selectedBeat, 1, keyNo);
+            let newSelectedBeat = parseInt(this.state.selectedBeat) + 1;
+            this.setState({song: newSong, selectedBeat: newSelectedBeat});
+        }
+
+        e.target.style.opacity = "75%";
+        setTimeout(function() { e.target.style.opacity = "0%"; }, 500);
+    }
+
+    
+    removeNoteFromSong(e) {
+        let start = e.target.classList[1].indexOf("-")+1;
+        let end = e.target.classList[1].length;
+        let keyNo = e.target.classList[1].substr(start, end);
+
+        let newSong = this.state.song;
+        newSong.splice(keyNo, 1);
+
+        this.setState({song: newSong});
+    }
+
+    selectBeat(e) {
+        let start = e.target.classList[1].indexOf("-")+1;
+        let end = e.target.classList[1].length;
+        let beatNo = e.target.classList[1].substr(start, end);
+
+        this.setState({selectedBeat: beatNo, startGroupSelectedBeat: null, endGroupSelectedBeat: null});
+    }
+
+    onGroupSelectStart(e) {
+        let start = e.target.classList[1].indexOf("-")+1;
+        let end = e.target.classList[1].length;
+        let startBeatNo = e.target.classList[1].substr(start, end);
+
+        this.setState({isGroupSelect: true, selectedBeat: null, startGroupSelectedBeat: startBeatNo, endGroupSelectedBeat: null});
+    }
+
+    onGroupSelectEnd(e) {
+        let start = e.target.classList[1].indexOf("-")+1;
+        let end = e.target.classList[1].length;
+        let endBeatNo = e.target.classList[1].substr(start, end);
+        
+        if (this.state.isGroupSelect === true & this.state.startGroupSelectedBeat !== endBeatNo) {
+            this.setState({isGroupSelect: false});
+            this.setState({endGroupSelectedBeat: endBeatNo});
+        } else {
+            this.setState({isGroupSelect: false});
+        }
+    }
+
+    
+}
+
+export default TuneCreator;
